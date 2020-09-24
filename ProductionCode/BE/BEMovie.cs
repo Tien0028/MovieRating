05b7dcb6187160e3c17172d71a69fee72d5d4182
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2020-09-24 13:45:25 +0200

    Further Changes

---
+++ ProductionCode/BE/BEMovie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductionCode.BEMovie
+{
+    public class BEMovie
+    {
+        public int Reviewer { get; set; }
+        public int Movie { get; set; }
+        public int Grade { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
