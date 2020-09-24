626e595bbcc6ece74cfe0ccd0adf7503cd92c122
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2020-09-24 14:05:20 +0200

    Changes

---
+++ ProductionCode/Core.Service/IDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductionCode.Core.Service
+{
+    interface IDataAccess
+    {
+        int GetNumberOfReviewsFromReviewer(int reviewer);
+        double GetAverageRateFromReviewer(int reviewer);
+        int GetNumberOfRatesByReviewer(int reviewer, int rate);
+        int GetNumberOfReviews(int movie);
+        double GetAverageRateOfMovie(int movie);
+        List<int> GetMoviesWithHighestNumberOfTopRates();
+        List<int> GetMostProductiveReviewers();
+        List<int> GetTopRatedMovies(int amount);
+        List<int> GetTopMoviesByReviewer(int reviewer);
+        List<int> GetReviewersByMovie(int movie);
+
+    }
+}
