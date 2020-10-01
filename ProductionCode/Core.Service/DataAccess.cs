077b835c57d3dbd7bc059a244723e4549b6972c3
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2020-10-01 11:41:38 +0200

    Push

--- ProductionCode/Core.Service/DataAccess.cs
+++ ProductionCode/Core.Service/DataAccess.cs
@@ -11,8 +11,6 @@ namespace ProductionCode.Core.Service
     public class DataAccess
     {
         private readonly List<BERating> _beRatings = new List<BERating>();
-        //private readonly List<BEReviewer> _beReviewer = new List<BEReviewer>();
-        //private readonly List<BEMovie> _beMovie = new List<BEMovie>();
         public DataAccess()
         {   
 
@@ -27,11 +25,6 @@ namespace ProductionCode.Core.Service
             
         }
 
-        //public Tuple<List<BERating>, List<BEReviewer>> MultiValue(List<BERating> _beRatings, List<BEReviewer> _beReviewer)
-        //{
-        //    return Tuple.Create(_beRatings, _beReviewer);
-        //}
-
         public double GetAverageRateFromReviewer(int reviewer)
         {
             return _beRatings
@@ -42,35 +35,38 @@ namespace ProductionCode.Core.Service
         }
         public int GetNumberOfRatesByReviewer(int reviewer, int rate)
         {
-            throw new Exception;
+            return _beRatings.Where(mr => mr.Reviewer == reviewer).Where(mr => mr.Rate == rate).Count();
         }
         public int GetNumberOfReviews(int movie)
         {
-            throw new Exception;
+            return _beRatings.Where(mr => mr.Movie == movie).Count();
         }
         public double GetAverageRateOfMovie(int movie)
         {
-            throw new Exception;
+            return _beRatings.Where(mr => mr.Movie == movie)
+                .Select(mr => mr.Rate)
+                .DefaultIfEmpty(0)
+                .Average();
         }
         public List<int> GetMoviesWithHighestNumberOfTopRates()
         {
-            throw new Exception;
+            return _beRatings.OrderByDescending(mr => mr.Rate).Select(mr => mr.Movie).Distinct().ToList(); 
         }
         public List<int> GetMostProductiveReviewers()
         {
-            throw new Exception;
+            return _beRatings.GroupBy(mr => mr.Reviewer).OrderByDescending(mr => mr.Count()).Select(mr => mr.Key).ToList();
         }
         public List<int> GetTopRatedMovies(int amount)
         {
-            throw new Exception;
+            throw new Exception();
         }
         public List<int> GetTopMoviesByReviewer(int reviewer)
         {
-
+            throw new Exception();
         }
         public List<int> GetReviewersByMovie(int movie)
         {
-
+            throw new Exception();
         }
 
 
