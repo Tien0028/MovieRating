ec4154ebc279a904567ea04ef787f9efe441d5f8
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-10-02 10:42:43 +0200

    Changes

---
+++ ProductionCode/MovieRating/IMovieRating.cs
@@ -0,0 +1,37 @@
+﻿using ProductionCode.BE;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductionCode.MovieRating
+{
+    public interface IMovieRating
+    {
+
+        HashSet<BERating> Ratings { get; set; }
+
+        int NumberOfReviews(int rID); // GetNumberOfReviewsFromReviewer
+
+        double AvgOfReviewer(int rID); //  GetAverageRateFromReviewer
+
+        int GradeCountByID(int rID, int grade); // GetNumberOfRatesByReviewer
+
+        int MovieReviewerCount(int mID); //  GetNumberOfReviews
+
+        double MovieReviewAvg(int mID); // GetAverageRateOfMovie
+
+        int MovieReviewByGrade(int mID, int grade);  //  GetNumberOfRates
+
+        List<int> MovieMostTopRate(); //  GetMoviesWithHighestNumberOfTopRates
+
+        List<int> ReviewerTopCount(); // GetMostProductiveReviewers
+
+        List<int> TopMovies(int num); // GetTopRatedMovies
+
+        List<int> RevieverMovies(int rID);  //  GetTopMoviesByReviewer
+
+        List<int> MovieReviewers(int mID); // GetReviewersByMovie
+
+
+    }
+}
---
+++ ProductionCode/MovieRating/MovieRating.cs
@@ -0,0 +1,49 @@
+﻿using ProductionCode.BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProductionCode.MovieRating
+{
+    public class MovieRating
+    {
+
+        public HashSet<BERating> Ratings { get; set; }
+
+        public double AvgOfReviewer(int rID)
+        {
+            double avg = Ratings.Where(x => x.Reviewer == rID).Average(x => x.Rate);
+            return avg;
+        }
+
+        public int GradeCountByID(int rID, int grade)
+        {
+            int count = Ratings.Where(x => x.Reviewer == rID && x.Rate == grade).Count();
+            return count;
+        }
+
+        public List<int> MovieMostTopRate()
+        {
+            var topRatedMoviesGrade = Ratings.Where(r => r.Rate == 5).Select(r => r.Movie).ToList();
+
+            return topRatedMoviesGrade;
+        }
+
+        public double MovieReviewAvg(int mID)
+        {
+            var averageRate = Ratings.Where(BERating => mID == BERating.Movie).Average(r => r.Rate);
+
+            return averageRate;
+
+        }
+
+        public int MovieReviewByGrade(int mID, int grade)
+        {
+            var gradeCounter = Ratings.Where(BERating => mID == BERating.Movie && BERating.Rate == grade).Count();
+
+            return gradeCounter;
+        }
+
+    }
+}
